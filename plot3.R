264cbaf95912ad57eeb3e0a82110f39ac1e2331a
rajnigoyal03 <rajnigoyal_03@yahoo.com>
2016-07-21 08:47:58 -0500

    Create plot3.R

---
+++ plot3.R
@@ -0,0 +1,27 @@
+Enter file contents here
+#Data Loading and Preprocessing
+
+dataFile <- "household_power_consumption.txt"
+power <- read.table(dataFile, header=T, sep=";")
+power$Date <- as.Date(power$Date, format="%d/%m/%Y")
+df <- power[(power$Date=="2007-02-01") | (power$Date=="2007-02-02"),]
+df$Global_active_power <- as.numeric(as.character(df$Global_active_power))
+df$Global_reactive_power <- as.numeric(as.character(df$Global_reactive_power))
+df$Voltage <- as.numeric(as.character(df$Voltage))
+df <- transform(df, timestamp=as.POSIXct(paste(Date, Time)), "%d/%m/%Y %H:%M:%S")
+df$Sub_metering_1 <- as.numeric(as.character(df$Sub_metering_1))
+df$Sub_metering_2 <- as.numeric(as.character(df$Sub_metering_2))
+df$Sub_metering_3 <- as.numeric(as.character(df$Sub_metering_3))
+
+#My Plot 3
+
+plot3 <- function() {
+        plot(df$timestamp,df$Sub_metering_1, type="l", xlab="", ylab="Energy sub metering")
+        lines(df$timestamp,df$Sub_metering_2,col="red")
+        lines(df$timestamp,df$Sub_metering_3,col="blue")
+        legend("topright", col=c("black","red","blue"), c("Sub_metering_1  ","Sub_metering_2  ", "Sub_metering_3  "),lty=c(1,1), lwd=c(1,1))
+        dev.copy(png, file="plot3.png", width=480, height=480)
+        dev.off()
+        cat("plot3.png has been saved in", getwd())
+}
+plot3()
